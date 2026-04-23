16149f2ddb215f5bb350fe79dfa08ae48e3e946a
ravivaniya <ravivaniya4911@gmail.com>
2026-04-23 15:19:26 +0530

    CORS update

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://app.tryjavaab.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
